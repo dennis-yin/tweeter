f8c0933008298b5f6cbf1ef12d12c74a7d28569f
dennis-yin <dennis.yin26@gmail.com>
2019-11-11 03:45:26 +0000

    fix the look of tweets and time since tweet creation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,20 +4,44 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const calculateTweetCreation = function(time) {
+  time /= 1000;  // Convert milliseconds to seconds
+
+  if (time < 60) {
+    return 'Less than a minute ago';
+  }
+  if (time < 3600) {
+    return Math.floor(time / 60) + ' minutes ago';
+  }
+  if (time < 86400) {
+    return Math.floor(time / 3600) + ' hours ago';
+  }
+  if (time < 2592000) {
+    return Math.floor(time / 86400) + ' days ago';
+  }
+  if (time < 31104000) {
+    return Math.floor(time / 2592000) + ' months ago';
+  }
+  return Math.floor(time / 31104000) + ' years ago';
+};
+
 const createTweetElement = function(tweet) {
+  const currDate = Date.now();
+  const dateCreated = new Date(tweet.created_at).getTime();
+  const dateDiff = currDate - dateCreated;
+
   const $tweet = $(`
   <article class="tweet">
     <header class="tweet-header">
-      <p>${tweet.user.name}</p>
-      <p>${tweet.user.handle}</p>
+      <img src=${tweet.user.avatars}>
+      <p class="username">${tweet.user.name}</p>
+      <p class="handle">${tweet.user.handle}</p>
     </header>
-
     <p class="tweet-content">
       ${escape(tweet.content.text)}
     </p>
-
     <footer class="tweet-footer">
-      <p>${tweet.created_at} days ago</p>
+      <p class="date-created">${calculateTweetCreation(dateDiff)}</p>
     </footer>
   `);
 
