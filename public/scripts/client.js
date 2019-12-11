f0d8b096c081a3be4b58bbc4c3ab8594eb83471c
Dennis Yin <dennis.yin26@gmail.com>
2019-12-11 23:26:25 +0000

    fix issues with placement of header, new tweet box, and tweet container in desktop mode

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,22 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 const calculateTweetCreation = function(time) {
-  time /= 1000;  // Convert milliseconds to seconds
+  time /= 1000; // Convert milliseconds to seconds
 
   if (time < 60) {
-    return 'Less than a minute ago';
+    return "Less than a minute ago";
   }
   if (time < 3600) {
-    return Math.floor(time / 60) + ' minutes ago';
+    return Math.floor(time / 60) + " minutes ago";
   }
   if (time < 86400) {
-    return Math.floor(time / 3600) + ' hours ago';
+    return Math.floor(time / 3600) + " hours ago";
   }
   if (time < 2592000) {
-    return Math.floor(time / 86400) + ' days ago';
+    return Math.floor(time / 86400) + " days ago";
   }
   if (time < 31104000) {
-    return Math.floor(time / 2592000) + ' months ago';
+    return Math.floor(time / 2592000) + " months ago";
   }
-  return Math.floor(time / 31104000) + ' years ago';
+  return Math.floor(time / 31104000) + " years ago";
 };
 
 const createTweetElement = function(tweet) {
@@ -51,8 +45,8 @@ const createTweetElement = function(tweet) {
   return $tweet;
 };
 
-const escape =  function(str) {
-  let div = document.createElement('div');
+const escape = function(str) {
+  let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
@@ -60,50 +54,62 @@ const escape =  function(str) {
 const renderTweets = function(tweets) {
   for (const tweet of tweets) {
     $newTweet = createTweetElement(tweet);
-    $('.tweet-container').prepend($newTweet); 
+    $(".tweet-container").prepend($newTweet);
   }
 };
 
 const loadTweets = function(cb) {
-  $.ajax('/tweets', { method: 'GET' })
-    .then(function(tweets) {
-      cb(tweets);
-    })
+  $.ajax("/tweets", { method: "GET" }).then(function(tweets) {
+    cb(tweets);
+  });
 };
 
 const toggleNewTweet = function(element) {
   element.click(() => {
-    const $newTweet = $('.new-tweet');
-    if ($newTweet.hasClass('hidden')) {
-      $newTweet.slideDown('fast').toggleClass('hidden');
+    const $newTweet = $(".new-tweet");
+    if ($newTweet.hasClass("hidden")) {
+      $newTweet
+        .slideDown("fast", () => {
+          $newTweet.find("textarea").focus();
+        })
+        .toggleClass("hidden");
     } else {
-      $newTweet.slideUp('fast').toggleClass('hidden');
+      $newTweet.slideUp("fast").toggleClass("hidden");
     }
   });
 };
 
 const submitTweet = function() {
-  $('#new-tweet-form').submit(function(event) {
+  $("#new-tweet-form").submit(function(event) {
     event.preventDefault();
 
     const $data = $(this).serialize();
-    const $userTextInput = $(this).find('textarea');
-    const $emptyError = $(this).parent().find('.empty-error')
-    const $lengthError = $(this).find('.length-error');
-  
-    console.log($emptyError.is(':hidden'))
-    if ($emptyError.is(':hidden')) {
-      console.log("YEAH")
-      if ($userTextInput.val() === '') { 
-        console.log("UH HUH")
-        $emptyError.slideDown('slow').toggleClass('hidden');
+    const $userTextInput = $(this).find("textarea");
+    const $emptyError = $(this)
+      .parent()
+      .find(".empty-error");
+    const $lengthError = $(this).find(".length-error");
+
+    if ($emptyError.is(":hidden")) {
+      if ($userTextInput.val() === "") {
+        $emptyError.slideDown("slow").toggleClass("hidden");
         return;
+      }
+    } else {
+      if ($userTextInput.val() !== "") {
+        $emptyError.slideUp("slow").toggleClass("hidden");
       }
     }
 
-    if ($lengthError.is(':hidden')) {
+    if ($lengthError.is(":hidden")) {
       if ($userTextInput.val().length > 140) {
-        $lengthError.slideDown('slow').toggleClass('hidden');
+        $lengthError.slideDown("slow").toggleClass("hidden");
+        return;
+      }
+    } else {
+      if ($userTextInput.val().length <= 140) {
+        $lengthError.slideUp("slow").toggleClass("hidden");
+      } else {
         return;
       }
     }
@@ -114,9 +120,9 @@ const submitTweet = function() {
       data: $data,
       success: function() {
         loadTweets(renderTweets);
-        $userTextInput.val('');
-        $('.counter').text('140');
-        $('.tweet-container').html(``);
+        $userTextInput.val("");
+        $(".counter").text("140");
+        $(".tweet-container").html(``);
       }
     });
   });
@@ -125,5 +131,5 @@ const submitTweet = function() {
 $(document).ready(function() {
   submitTweet();
   loadTweets(renderTweets);
-  toggleNewTweet($('.write-tweet'));
+  toggleNewTweet($(".write-tweet"));
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,22 @@
-$(document).ready(function() {
-  $(".new-tweet textarea").keypress(function() {
-    const $counter = $(this).parent().find('.counter');
-    $counter.text(140 - $(this).val().length - 1);
-    $counter.css({
+const getCharsRemaining = function() {
+  const tweetBox = $("#tweet-box");
+  const counter = $("#counter");
+
+  tweetBox.on("input", () => {
+    counter.text(140 - tweetBox.val().length);
+
+    counter.css({
       color: function() {
-        if ($counter.text() < "0") {
+        if (counter.text() < "0") {
           return "red";
+        } else {
+          return "#545159";
         }
       }
     });
   });
+};
+
+$(document).ready(() => {
+  getCharsRemaining();
 });
